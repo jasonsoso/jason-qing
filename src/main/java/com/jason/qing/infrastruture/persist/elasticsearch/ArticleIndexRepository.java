a7faccfb80ab2e204bb3f1151697aa01d63b0c17
jasonsoso <648636045@qq.com>
2014-03-10 21:07:21 +0800

    es test

--- src/main/java/com/jason/qing/infrastruture/persist/elasticsearch/ArticleIndexRepository.java
+++ src/main/java/com/jason/qing/infrastruture/persist/elasticsearch/ArticleIndexRepository.java
@@ -1,6 +1,16 @@
 package com.jason.qing.infrastruture.persist.elasticsearch;
 
 
-public interface ArticleIndexRepository  {//extends ElasticsearchRepository<ArticleIndex,String>
-	
+import com.jason.qing.domain.article.Article;
+
+
+public interface ArticleIndexRepository {
+
+	//Article get(String id);
+
+	void index(Article entity);
+
+	//List<Article> query(String queryString);
+
+	//Page<Article> queryPage(Page<Article> page, String queryString);
 }
